0a5cdb5dd138098cbc2ea7585d9738a7d6ba9947
OleksandrKulchytskyi <sashaas@inbox.ru>
2014-04-14 11:42:14 +0300

    First commit

---
+++ Practice.Client/Practice.Common/Entities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.Common
+{
+	public interface IEntity
+	{
+		int Id { get; set; }
+	}
+
+	public class Trunk:IEntity
+	{
+
+		public int Id
+		{
+			get;
+			set;
+		}
+
+		public string Name { get; set; }
+
+		public string Address { get; set; }
+
+		public int CarCount { get; set; }
+
+		public List<Car> Cars { get; set; }
+	}
+
+
+	public class Car : IEntity
+	{
+		
+	}
+}
